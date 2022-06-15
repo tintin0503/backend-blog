79e1a1840f84de48f5681e5c59da155464dca9d3
tintin0503 <quanptit2801@gmail.com>
2022-06-15 07:17:12 +0700

    first commit

---
+++ src/main/java/tintin/com/backendblog/BackendBlogApplication.java
@@ -0,0 +1,13 @@
+package tintin.com.backendblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tintin/com/backendblog/BackendBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package tintin.com.backendblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
